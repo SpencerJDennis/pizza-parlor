47973c73345c5d619957ea9a73c08faec48c98b2
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-26 16:56:13 -0700

    add pizzaForm function WIP - forgot to commit earlier sorry about that I promise I was working though lol

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,17 @@ function Pizza(toppings = [], size) {
   this.size = size
 }
 
+function pizzaForm(){
+  let topping1 = document.getElementById("ham").value;
+  let topping2 = document.getElementById("spam").value;
+  let topping3 = document.getElementById("yam").value;
+  let size = document.getElementById("size").value
+  let newPizza = new Pizza([topping1, topping2, topping3], size)
+
+    if (topping1)
+}
+
+
 window.addEventListener("load", function() {
   const form = document.getElementById("pizza-options")
   form.addEventListener("submit", )
