3e4f8bdd7cfc7fec0645c19da0b14cb8a1a6afae
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-26 13:57:52 -0700

    add submit button for form and window event listener

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,8 @@ function Pizza(toppings = [], size) {
   this.toppings = toppings
   this.size = size
 }
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("pizza-options")
+  form.addEventListener("submit", )
+})
